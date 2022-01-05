3476f2577da984987cc861d09f7bcd031d2aeb25
Daniel Thomas <dansebtom@yahoo.co.uk>
2022-01-05 22:41:59 +0000

    Create functionality for server to receive a username from a client

--- server/Server.js
+++ server/Server.js
@@ -1,13 +1,29 @@
 
-let http = require('http').createServer();
+let http = require("http").createServer();
 let port = 9000;
 http.listen(port, function(){console.log("Hosting on port " + port)})
-let io = require('socket.io')(http,{
+let io = require("socket.io")(http,{
     cors: { origin: "*"}
 })
 
+let registeredUsers = [];
 
-io.on('connection', function(socket){
-    console.log("User " + socket.id + " connection made") 
 
+io.on("connection", function(socket){
+    
+    console.log("Connection Made")
+
+    socket.on("RegisterUser", function(userdata)
+    {
+        console.log("User " + userdata + " connected");
+        registeredUsers.push(userdata);
+    });
+
+    socket.on('disconnect', function() 
+    {    
+        console.log('user disconnected');  
+    });
+    
+  
 });
+
